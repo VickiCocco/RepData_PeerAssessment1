8707d9ee14405b6f28ce7da410a7a9a463311a18
VickiCocco <vickicocco@gmail.com>
2017-03-28 22:07:29 -0400

    plot 2 done

---
+++ Plot1.R
@@ -0,0 +1,37 @@
+plot1 <- 
+        
+        # prepare the data
+        tblsteps <- tblactivity %>%
+        select(steps,
+               date,
+               interval) %>%
+        filter(steps != "NA") %>%
+        mutate(intervaldate = as.Date(date, format = "%Y-%m-%d")) %>%
+        arrange(intervaldate) %>%
+        group_by(intervaldate) %>%
+        summarise(daysteps = sum(steps))
+
+        # plot to a pdf file
+        #pdf("issues.pdf", width = 7, height = 9)
+        
+        # plot the histogram of steps each day
+        plot1 <- ggplot(tblsteps, aes(daysteps))
+        plot1 <- plot1 + geom_histogram(bins = 25, aes(fill = ..count..))
+        plot1 <- plot1 + labs(x = "Number of Steps per Day" ,
+                              y = "How Many Days at Each Count", 
+                              title = "Distribution of Number of Steps per Day",
+                              subtitle = "2012")
+        plot1 <- plot1 + theme(plot.title=element_text(size=14, 
+                                                       hjust=0.5, 
+                                                       face="bold", 
+                                                       colour="darkorchid4", 
+                                                       vjust=-1)) 
+        plot1 <- plot1 + theme(plot.subtitle=element_text(size=10, 
+                                                       hjust=0.5, 
+                                                       face="bold", 
+                                                       colour="black", 
+                                                       vjust=-1)) 
+        
+        
+        print(plot1)
+        #dev.off() 
