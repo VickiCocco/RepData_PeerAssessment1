e0792d4223dfb8e86aeca9db884477dfbbf675ca
VickiCocco <vickicocco@gmail.com>
2017-03-30 22:19:29 -0400

    random

--- Plot4.R
+++ Plot4.R
@@ -1,6 +1,21 @@
 plot4 <- 
-        
-        tblsteps <- mutate(tblcomplete, dayofweek = weekdays(intervaldate))
 
-        weekend <- c("Saturday", "Sunday")
 
+        tbldaysteps <- tblcomplete %>%
+                select(steps,
+                       interval,
+                       date) %>%
+                mutate(intervaldate = as.Date(date, format = "%Y-%m-%d"))
+
+        tbldaysteps <- mutate(tbldaysteps, datetype = ifelse(weekdays(intervaldate)=="Saturday" | weekdays(intervaldate)=="Sunday", "weekend", "weekday"))
+
+                tbldaysteps <- tbldaysteps %>%
+                select(steps,
+                       interval, 
+                       intervaldate) %>%
+                mutate(datetype =  ifelse(weekdays(intervaldate)=="Saturday" | weekdays(intervaldate)=="Sunday", "weekend", "weekday")) %>%
+                arrange(datetype, interval) %>%
+                group_by(datetype, interval) %>%
+                summarise(meansteps = mean(steps))
+
+                
